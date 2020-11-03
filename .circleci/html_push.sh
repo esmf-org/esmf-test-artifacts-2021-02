75b1d054aeebf577355203f50158a5d002d8cd54
Himanshu Pillai <himanshupillai@gmail.com>
2020-11-03 12:32:03 -0700

    bash script to push html to esmf-org.github.io

---
+++ .circleci/html_push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail
+
+git config --global user.email "himanshu@ucar.edu"
+git config --global user.name "esmf-orgbot"
+
+cd
+git clone --depth 1 git@github.com:esmf-org/esmf-org.github.io.git
+cd ~/esmf-org.github.io/regression_test_result/nightly/develop/
+cp -rf /tmp/esmf-test-artifacts/develop/index.html .
+cd ~/esmf-org.github.io/
+git add .
+git commit -m "html result table pushed by CircleCI to esmf-org.github.io"
+git push origin master
