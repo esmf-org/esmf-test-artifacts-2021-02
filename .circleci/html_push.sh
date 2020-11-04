b859717ab8b85c7a14a3a3077e70ad99fa4b04bd
Himanshu Pillai <himanshupillai@gmail.com>
2020-11-04 00:55:32 -0700

    Update html_push.sh

--- .circleci/html_push.sh
+++ .circleci/html_push.sh
@@ -6,10 +6,11 @@ git config --global user.name "esmf-orgbot"
 
 cd
 pwd
-git clone --depth 1 git@github.com:esmf-org/esmf-org.github.io.git
+git clone git@github.com:esmf-org/esmf-org.github.io.git
 cd esmf-org.github.io/regression_test_result/nightly/develop/
 cp -rf ../../../../project/develop/index.html .
 cd  ../../..
 git add .
 git commit -m "html result table pushed by CircleCI to esmf-org.github.io"
+git remote prune origin
 git push origin master
