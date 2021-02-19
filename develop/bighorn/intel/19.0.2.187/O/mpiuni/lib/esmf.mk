d93913af1aee2cb4a415cda500f299827642d27f
Himanshu Pillai <himanshu@bighorn.cgd.ucar.edu>
2021-02-19 05:21:12 -0700

    Bighorn Intel artifacts pushed at Fri Feb 19 05:21:11 MST 2021

--- develop/bighorn/intel/19.0.2.187/O/mpiuni/lib/esmf.mk
+++ develop/bighorn/intel/19.0.2.187/O/mpiuni/lib/esmf.mk
@@ -12,12 +12,12 @@
 #
 
 #----------------------------------------------
-ESMF_VERSION_STRING=8.1.0 beta snapshot
-ESMF_VERSION_STRING_GIT=ESMF_8_1_0_beta_snapshot_46-5-g3a89871faa
+ESMF_VERSION_STRING=8.2.0 beta snapshot
+ESMF_VERSION_STRING_GIT=ESMF_8_2_0_beta_snapshot_00
 #----------------------------------------------
 
 ESMF_VERSION_MAJOR=8
-ESMF_VERSION_MINOR=1
+ESMF_VERSION_MINOR=2
 ESMF_VERSION_REVISION=0
 ESMF_VERSION_PATCHLEVEL=0
 ESMF_VERSION_PUBLIC='F'
@@ -33,7 +33,7 @@ ESMF_F90LINKER=ifort
 
 ESMF_F90COMPILEOPTS=-O -fPIC -assume realloc_lhs -m64 -mcmodel=small -pthread -threads  -qopenmp
 ESMF_F90COMPILEPATHS=-I/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf/mod/modO/Linux.intel.64.mpiuni.default -I/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf/src/include 
-ESMF_F90COMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMFVERSIONGIT='ESMF_8_1_0_beta_snapshot_46-5-g3a89871faa' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf -DESMF_MPIUNI
+ESMF_F90COMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMFVERSIONGIT='ESMF_8_2_0_beta_snapshot_00' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf -DESMF_MPIUNI
 ESMF_F90COMPILEFREECPP=
 ESMF_F90COMPILEFREENOCPP=
 ESMF_F90COMPILEFIXCPP=
@@ -52,7 +52,7 @@ ESMF_CXXLINKER=icpc
 
 ESMF_CXXCOMPILEOPTS=-std=c++11 -O -DNDEBUG -fPIC -m64 -mcmodel=small -pthread  -qopenmp
 ESMF_CXXCOMPILEPATHS= -I/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf/src/include   -I/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf/src/Infrastructure/stubs/mpiuni -I/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf/src/prologue/yaml-cpp/include
-ESMF_CXXCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMFVERSIONGIT='ESMF_8_1_0_beta_snapshot_46-5-g3a89871faa' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf -D__SDIR__='' -DESMF_CXXSTD=11 -DESMF_MPIUNI
+ESMF_CXXCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMFVERSIONGIT='ESMF_8_2_0_beta_snapshot_00' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf -D__SDIR__='' -DESMF_CXXSTD=11 -DESMF_MPIUNI
 
 ESMF_CXXLINKOPTS=  -m64 -mcmodel=small -pthread -Wl,--no-as-needed  -qopenmp
 ESMF_CXXLINKPATHS=-L/scratch/esmf/esmf_test/cronjobs/trunk/intel/esmf/lib/libO/Linux.intel.64.mpiuni.default  -L/usr/local/intel-cluster-19.0.2/compilers_and_libraries_2019.2.187/linux/compiler/lib/intel64_lin/
